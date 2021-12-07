17ef2c8a970bbb939fe208c137a0409ed7421b3e
P-Harvey <62023010+P-Harvey@users.noreply.github.com>
2021-12-07 10:16:35 -0500

    Update JADESOUND_STATS.py

--- JADESOUND_STATS.py
+++ JADESOUND_STATS.py
@@ -2,14 +2,31 @@
 # 20211207
 
 import numpy as np
+import pandas as pd
 import scipy as sp
-import statsmodels as sm
+import statsmodels.api as sm
+import statsmodels.stats as smstats
 
 def leastSquares(X, y):
-    olsFit = sm.OLS(y, X).fit()
+    olsFit = sm.regression.linear_model.OLS(y, X).fit()
     return olsFit
 
 def summStat(r):
     print(r.summary())
     return
 
+def descrStats(data):
+    descr = smstats.descriptivestats.describe(data)
+    return descr
+
+def test():
+    d = [1,2,3,4,5,6,7,8,9,10]
+    d1 = [10,9,8,7,6,5,4,3,2,1]
+    dataframe = {'X':d,"y":d1}
+    df = pd.DataFrame(dataframe)
+    fit = leastSquares(d, d1)
+    summStat(fit)
+    descriptions = descrStats(df)
+    print(descriptions)
+    
+test()
