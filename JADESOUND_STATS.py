72e4c6fff1eae02f6e0af88178bdab40c48221d8
n0lley <ntolley13@gmail.com>
2021-12-12 23:18:09 -0500

    saving for the night, changed data structures to panda dataframes because that's so much better on every level.

--- JADESOUND_STATS.py
+++ JADESOUND_STATS.py
@@ -9,6 +9,7 @@ import statsmodels.api as sm
 import statsmodels.stats as smstats
 import os
 import csv
+from state_abr import abr
 
 def leastSquares(X, y):
     olsFit = sm.regression.linear_model.OLS(y, X).fit()
@@ -21,67 +22,14 @@ def summStat(r):
 def descrStats(data):
     descr = smstats.descriptivestats.describe(data)
     return descr
-    
-def loadcsv(filename):
-    """
-    Load CSV file into list-of-dictionaries format
-    Input: string name of file to be loaded
-    Output: list of observations contained in file
-    """
-    data = []
-    if filename in os.listdir("data/") and filename.endswith(".csv"):
-        f = open("data/"+filename)
-        reader = csv.reader(f)
-        keys = []
-        for row in reader:
-            if keys == []:
-                keys = [key.strip() for key in row]
-            
-            else:
-                entry= {}
-                for key, value in zip(keys, row):
-                    entry[key] = value
-                data.append(entry)
-    
-    if data == []: print("Error: No data acquired.")
-    return data
-    
         
-def extract_subset(data, keys):
-    """
-    For pulling subsets of variable:value pairs from larger observation sets.
-    Inputs:
-            data: dataset in the form of a list of {variable:value} dictionaries
-            keys: variables to be extracted into the subset
-    Output: list of {variable:value} dictionaries containing just the variables in keys
-    """
-    subSet = []
-    for obs in data:
-        subObs = dict.fromkeys(keys)
-        for key in keys:
-            subObs[key] = obs[key]
-        subSet.append(subObs)
+def calc_percents(data, pop_data, variable):
     
-    return subSet
-    
-def extract_by_value(data, key, value):
-    """
-    For extracting every observation whose specified key has the specified value
-    """
-    subSet = []
-    for obs in data:
-        if obs[key] == value:
-            subSet.append(obs)
-    
-    return subSet
-    
-def sort_by_key(observation, key=""):
-    """
-    General skeleton function for sorting list of dictionaries by a specific key.
-    We can copy and modify to make it more task specific as needed.
-    Call in the "key" argument of sorted() as sort_by_key(key=X)
-    """
-    return observation[key]
+    outdata = pd.Dataframe(columns=["State":abr.values(), "Percentage", "Population"])
+    for state in abr.values():
+        total_pop = pop_data.loc[pop_data["State"] == state].sum()
+        
+        
 
 def test():
     d = [1,2,3,4,5,6,7,8,9,10]
@@ -94,24 +42,19 @@ def test():
     print(descriptions)
     
 def main():
-    indicator_seven_days = loadcsv("Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
-    indicator_four_weeks = loadcsv("Indicators_of_Reduced_Access_to_Care_Due_to_the_Coronavirus_Pandemic_During_Last_4_Weeks.csv")
-    state_and_county = loadcsv ("StateAndCountyData.csv")
-    variable_list = loadcsv("VariableList.csv")
-    
-    #get code meanings, swap them in for the state and county observations
-    code_meanings = {}
-    variable_correspondences = extract_subset(variable_list, ["\ufeffVariable_Name", "Variable_Code"])
-    for code in variable_correspondences:
-        variable_code = code["Variable_Code"]
-        variable_name = code["\ufeffVariable_Name"]
-        code_meanings[variable_code] = variable_name
+    indicator_seven_days = pd.read_csv("data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
+    indicator_four_weeks = pd.read_csv("data/Indicators_of_Reduced_Access_to_Care_Due_to_the_Coronavirus_Pandemic_During_Last_4_Weeks.csv")
+    state_and_county = pd.read_csv("data/StateAndCountyData.csv")
+    variable_list = pd.read_csv("data/VariableList.csv")
+    county_supplement = pd.read_csv("data/SupplementalDataCounty.csv")
     
     #Trim to just data by state
-    indicator_seven_days = extract_by_value(indicator_seven_days, "Group", "By State")
-    indicator_four_weeks = extract_by_value(indicator_four_weeks, "Group", "By State")
+    indicator_seven_days = indicator_seven_days.loc[indicator_seven_days["Group"] == "By State"]
+    indicator_four_weeks = indicator_seven_days.loc[indicator_seven_days["Group"] == "By State"]
     
-    #populations of counties in 2015
-    county_populations = extract_by_value(state_and_county, 
-    
+    pop_data = county_supplement.loc[county_supplement["Variable_Code"] == "Population_Estimate_2015"]
+    for county in pop_data["County"]:
+        county = county[0:-7]
+
+
 main()
