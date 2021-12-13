e95d92995187953b6d6a9160b025b5606c57f842
P-Harvey <62023010+P-Harvey@users.noreply.github.com>
2021-12-13 09:07:15 -0500

    Updated calc_percents and state_abr

--- JADESOUND_STATS.py
+++ JADESOUND_STATS.py
@@ -1,6 +1,5 @@
 # JADESOUND FP
 # 20211207
-
 import numpy as np
 import pandas as pd
 import scipy as sp
@@ -22,14 +21,17 @@ def summStat(r):
 def descrStats(data):
     descr = smstats.descriptivestats.describe(data)
     return descr
-        
+
 def calc_percents(data, pop_data, variable):
-    
-    outdata = pd.Dataframe(columns=["State":abr.values(), "Percentage", "Population"])
+
+    outdata = pd.DataFrame(columns=["State", "Percentage", "Population"])
     for state in abr.values():
         total_pop = pop_data.loc[pop_data["State"] == state].sum()
-        
-        
+        pop_count = data.loc[data["State"] == state].sum()
+        perc_pop = pop_count/total_pop
+        new_line = {"State":state, "Percentage":perc_pop, "Population":perc_pop}
+        outdata.append(new_line)
+    return outdata
 
 def test():
     d = [1,2,3,4,5,6,7,8,9,10]
@@ -40,18 +42,18 @@ def test():
     summStat(fit)
     descriptions = descrStats(df)
     print(descriptions)
-    
+
 def main():
     indicator_seven_days = pd.read_csv("data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv")
     indicator_four_weeks = pd.read_csv("data/Indicators_of_Reduced_Access_to_Care_Due_to_the_Coronavirus_Pandemic_During_Last_4_Weeks.csv")
     state_and_county = pd.read_csv("data/StateAndCountyData.csv")
     variable_list = pd.read_csv("data/VariableList.csv")
     county_supplement = pd.read_csv("data/SupplementalDataCounty.csv")
-    
+
     #Trim to just data by state
     indicator_seven_days = indicator_seven_days.loc[indicator_seven_days["Group"] == "By State"]
     indicator_four_weeks = indicator_seven_days.loc[indicator_seven_days["Group"] == "By State"]
-    
+
     pop_data = county_supplement.loc[county_supplement["Variable_Code"] == "Population_Estimate_2015"]
     for county in pop_data["County"]:
         county = county[0:-7]
